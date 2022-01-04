b1d791f67f05bee8d2d6b4f198509a3e92bb7aea
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-01-04 15:48:48 -0700

    chap 1 and 2 stuff

---
+++ book/chapter2/Basics/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main (string[] args)
+        { // the start of a block
+            Console.WriteLine("Hellow World!"); //a statement
+        } //the end of a block
+    }
+}
