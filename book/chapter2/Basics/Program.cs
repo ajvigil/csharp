bb19949197513f5d7a719f84d5b3eff43ae695a0
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-03-01 11:34:09 -0700

    chapter 2 stuff

--- book/chapter2/Basics/Program.cs
+++ book/chapter2/Basics/Program.cs
@@ -1,13 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Basics
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main (string[] args)
-        { // the start of a block
-            Console.WriteLine("Hellow World!"); //a statement
-        } //the end of a block
+      // declare some unused variables using types
+      // in additional assemblies
+      System.Data.DataSet ds;
+      System.Net.Http.HttpClient client;
+
+      // loop through the assemblies that this app references 
+      foreach (var r in Assembly.GetEntryAssembly()
+        .GetReferencedAssemblies())
+      {
+        // load the assembly so we can read its details
+        var a = Assembly.Load(new AssemblyName(r.FullName));
+
+        // declare a variable to count the number of methods
+        int methodCount = 0;
+
+        // loop through all the types in the assembly 
+        foreach (var t in a.DefinedTypes)
+        {
+          // add up the counts of methods 
+          methodCount += t.GetMethods().Count();
+        }
+        // output the count of types and their methods
+        Console.WriteLine(
+          "{0:N0} types with {1:N0} methods in {2} assembly.",
+          arg0: a.DefinedTypes.Count(),
+          arg1: methodCount,
+          arg2: r.Name);
+      }
     }
+  }
 }
+
+/*
+this is a multi-line 
+comment. 
+*/
+
