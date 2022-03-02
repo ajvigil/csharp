5c07c858b656f4707a9b3b5739c7eb62e9d31a96
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-03-02 07:58:40 -0700

    arrays and nulls

---
+++ book/chapter2/NullHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Arrays
+{
+  class Program
+  {
+      static void Main(string[] args)
+      {
+        //   int thisCannotBeNull = 4;
+        //   thisCannotBeNull = null; //compile error
+
+          int? thisCouldBeNull = null;
+          Console.WriteLine(thisCouldBeNull);
+          Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+          thisCouldBeNull = 7;
+          Console.WriteLine(thisCouldBeNull);
+          Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+          //when you run it shows 077
+          //the first line is blank because it is outputting the null value
+
+      }
+  }
+}
