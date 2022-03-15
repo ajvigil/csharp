94b1f98b19192a03a9f73d6ea9b8dd419a3b2725
Alicia Vigil <90734184+ajvigil@users.noreply.github.com>
2022-03-15 12:04:22 -0600

    setting up new console app

---
+++ book/chapter8/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+using static System.Console;
+using System.Text.RegularExpressions;
+
+namespace WorkingWithRegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checking for digits entered as text
+        }
+    }
+}
